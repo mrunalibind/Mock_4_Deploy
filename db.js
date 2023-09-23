221b92c01a2b62e39a491bb091177dca91df629e
Mrunali Bind <112799850+mrunalibind@users.noreply.github.com>
2023-09-24 00:14:31 +0530

    Done2

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 let mongoose=require("mongoose");
 // require("dotenv").config();
 let connection=mongoose.connect("mongodb+srv://mrunali:mrunalibind@cluster0.tsxywrf.mongodb.net/Mock_4?retryWrites=true&w=majority");
-
+// Cors
 module.exports={connection};
